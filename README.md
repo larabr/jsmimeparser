6cee388f3a7e2f72f21b9ad58ef305883768490c
larabr <7375870+larabr@users.noreply.github.com>
2022-08-16 13:49:59 +0200

    Integrate changes from comm-central (incl. UTF-7 support), switch to ES6 imports, use karma for testing

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+This is a fork of [mozilla-comm/jsmime](https://github.com/mozilla-comm/jsmime) that has been updated to include the changes made in [mozilla/releases-comm-central](https://github.com/mozilla/releases-comm-central/tree/master/mailnews/mime/jsmime) (incl. UTF-7 support).
+Further, the library now uses ES6 modules.  
+
+
 Code Layout
 ===========
 
@@ -19,6 +23,7 @@ Dependencies
 ============
 
 This code depends on the following ES6 features and Web APIs:
+* ES6 imports
 * ES6 generators
 * ES6 Map and Set
 * ES6 @@iterator support (especially for Map and Set)
@@ -28,32 +33,3 @@ This code depends on the following ES6 features and Web APIs:
 * Typed arrays (predominantly Uint8Array)
 * btoa, atob (found on global Windows or WorkerScopes)
 * TextDecoder
-
-Versions and API stability
-==========================
-
-As APIs require some use and experimentation to get a feel for what works best,
-the APIs may change between successive version updates as uses indicate
-substandard or error-prone APIs. Therefore, there will be no guarantee of API
-stability until version 1.0 is released.
-
-This code is being initially developed as an effort to replace the MIME library
-within Thunderbird. New versions will be released as needed to bring new support
-into the Thunderbird codebase; version 1.0 will correspond to the version where
-feature-parity with the old MIME library is reached. The set of features which
-will be added before 1.0 are the following:
-* S/MIME encryption and decryption
-* PGP encryption and decryption
-* IMAP parts-on-demand support
-* Support for text/plain to HTML conversion for display
-* Support for HTML downgrading and sanitization for display
-* Support for all major multipart types
-* Ability to convert HTML documents to text/plain and multipart/related
-* Support for building outgoing messages
-* Support for IDN and EAI
-* yEnc and uuencode decoding support
-* Support for date and Message-ID/References-like headers
-
-Other features than these may be added before version 1.0 is released (most
-notably TNEF decoding support), but they are not considered necessary to release
-a version 1.0.
--- test/test_custom_headers.js
+++ test/test_custom_headers.js
@@ -1,69 +1,41 @@
-"use strict";
-define(function (require) {
 
-var assert = require('assert');
-var jsmime = require('jsmime');
+import { assert } from "chai";
+import { headeremitter, headerparser } from "../lib";
 
-function arrayTest(data, fn) {
-  fn.toString = function () {
-    let text = Function.prototype.toString.call(this);
-    text = text.replace(/data\[([0-9]*)\]/g, function (m, p) {
-      return JSON.stringify(data[p]);
-    });
-    return text;
-  };
-  return test(data[0], fn);
-}
-
-function testHeader(header, tests) {
-  suite(header, function () {
-    tests.forEach(function (data) {
-      arrayTest(data, function () {
-        array.deepEqual(headerparser.parseStructuredHeader(header,
-          data[0]), data[1]);
-      });
-    });
-  });
-}
-
-function makeCT(media, sub, params) {
-  var object = new Map();
-  object.mediatype = media;
-  object.subtype = sub;
-  object.type = media + "/" + sub;
-  for (let k in params)
-    object.set(k, params[k]);
-  return object;
-}
-suite('Custom decoder support', function () {
+describe("Custom decoder support", function() {
   function customDecoder(values) {
-    let value = values.join('');
+    let value = values.join("");
     return atob(value);
   }
   function customEncoder(value) {
     this.addText(btoa(value), true);
   }
-  test('addStructuredEncoder', function () {
-    assert.equal('X-Base64: String\r\n',
-      jsmime.headeremitter.emitStructuredHeader('X-Base64', 'String', {}));
-    jsmime.headeremitter.addStructuredEncoder('X-Base64', customEncoder);
-    assert.equal('X-Base64: U3RyaW5n\r\n',
-      jsmime.headeremitter.emitStructuredHeader('X-Base64', 'String', {}));
-    assert.equal('X-Base64: U3RyaW5n\r\n',
-      jsmime.headeremitter.emitStructuredHeader('x-bASe64', 'String', {}));
+  it("addStructuredEncoder", function() {
+    assert.equal(
+      "X-Base64: String\r\n",
+      headeremitter.emitStructuredHeader("X-Base64", "String", {})
+    );
+    headeremitter.addStructuredEncoder("X-Base64", customEncoder);
+    assert.equal(
+      "X-Base64: U3RyaW5n\r\n",
+      headeremitter.emitStructuredHeader("X-Base64", "String", {})
+    );
+    assert.equal(
+      "X-Base64: U3RyaW5n\r\n",
+      headeremitter.emitStructuredHeader("x-bASe64", "String", {})
+    );
   });
-  test('addStructuredDecoder', function () {
-    assert.throws(function () {
-      jsmime.headerparser.parseStructuredHeader('X-Base64', 'U3RyaW5n');
-    });
-    jsmime.headerparser.addStructuredDecoder('X-Base64', customDecoder);
-    assert.equal('String',
-      jsmime.headerparser.parseStructuredHeader('X-Base64', 'U3RyaW5n'));
-    assert.throws(function () {
-      jsmime.headerparser.addStructuredDecoder('To', customDecoder);
-    });
+  it("addStructuredDecoder", function() {
+    assert.throws(function() {
+      headerparser.parseStructuredHeader("X-Base64", "U3RyaW5n");
+    }, /Unknown structured header/);
+    headerparser.addStructuredDecoder("X-Base64", customDecoder);
+    assert.equal(
+      "String",
+      headerparser.parseStructuredHeader("X-Base64", "U3RyaW5n")
+    );
+    assert.throws(function() {
+      headerparser.addStructuredDecoder("To", customDecoder);
+    }, /Cannot override header/);
   });
 });
-
-});
-
